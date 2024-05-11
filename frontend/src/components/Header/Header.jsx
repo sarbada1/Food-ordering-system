9f78a64741e0ca0b1517062997289ea2052599b3
sarbada1 <sanjelsarbada12@gmail.com>
2024-05-11 08:40:08 +0545

    home page design

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis deserunt asperiores nam, officiis consequuntur, ex vitae placeat voluptatem </p>
+        <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
